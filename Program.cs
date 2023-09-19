9a573d216d79ba4825f6c65e21a786a0fc722b3e
Ahmad Yazid Munif <ahmadyazid76@gmail.com>
2023-09-19 13:15:30 +0700

    Insert and Select by ID

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -15,8 +16,10 @@ namespace BasicConnectivity
         static void Main()
         {
 
-            //ConnectDB();
-            GetAllRegions();
+            ConnectDB();
+            //InsertRegion("Australia");
+            GetRegionById(21);
+            //GetAllRegions();
         }
         // Method connectDB untuk mengecek koneksi ke database apakah berhasil atau tidak;
         public static void ConnectDB()
@@ -65,40 +68,91 @@ namespace BasicConnectivity
                 Console.WriteLine($"Error: {ex.Message}");
             }
         }
-        /*
-        public static void GetAllRegions()
+        // INSERT: Region
+        // masukan data ke dalam tabel region
+        public static void InsertRegion(string name)
         {
-            connection = new SqlConnection(connectionString);
-            SqlCommand command = new SqlCommand();
+            using var connection = new SqlConnection(connectionString);
+            using var command = new SqlCommand();
+
             command.Connection = connection;
-            command.CommandText = "SELECT * FROM regions";
+            command.CommandText = "INSERT INTO regions VALUES (@name);";
+
+            try
+            {
+                var pName = new SqlParameter();
+                pName.ParameterName = "@name";
+                pName.Value = name;
+                pName.SqlDbType = SqlDbType.VarChar;
+                command.Parameters.Add(pName);
+
+                connection.Open();
+                using var transaction = connection.BeginTransaction();
+                try
+                {
+                    command.Transaction = transaction;
+
+                    var result = command.ExecuteNonQuery();
+
+                    transaction.Commit();
+                    connection.Close();
+
+                    switch (result)
+                    {
+                        case >= 1:
+                            Console.WriteLine("Insert Success");
+                            break;
+                        default:
+                            Console.WriteLine("Insert Failed");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    Console.WriteLine($"Error Transaction: {ex.Message}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        // GET BY ID: Region
+        public static void GetRegionById(int id) {
+            using SqlConnection connection = new SqlConnection(connectionString);
+            using SqlCommand command = new SqlCommand();
+
+            command.Connection = connection;
+            command.CommandText = "SELECT * FROM regions WHERE id="+id;
 
             try
             {
                 connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-
+                using var reader = command.ExecuteReader();
                 if (reader.HasRows)
-                {
                     while (reader.Read())
                     {
                         Console.WriteLine("Id: " + reader.GetInt32(0));
                         Console.WriteLine("Name: " + reader.GetString(1));
                     }
-                }
                 else
-                {
-                    Console.WriteLine("Not Found");
-                }
-
+                    Console.WriteLine("No rows found.");
                 reader.Close();
                 connection.Close();
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"error:{ex.Message}");
-            };
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
-        */
+                
+
+        // UPDATE: Region
+        public static void UpdateRegion(int id, string name) { }
+
+        // DELETE: Region
+        public static void DeleteRegion(int id) { }
     }
 }
